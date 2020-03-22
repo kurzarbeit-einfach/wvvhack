0c50b8e0caa2be2261f22c58f6c71bf121741816
Container 1845 cc-1845@jcdn.de <script@jcdn.de>
2020-03-22 21:53:08 +0100

    Auto-Commit

--- deploy_all_gits.sh
+++ deploy_all_gits.sh
@@ -3,6 +3,11 @@
 rsync -avz --exclude '.git' /repos/udo-pdf/ /git/pdf
 rsync -avz --exclude '.git' --exclude 'config' --exclude 'db' --exclude 'deployment' --exclude 'httpdocs/logs' /git/ /github
 
+cd /git
+git add *
+git commit -am "Auto-Commit"
+git push
+
 cd /github
 git add *
 git commit -am "Auto-Commit"
